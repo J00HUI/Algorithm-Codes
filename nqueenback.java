a2cf5f9bc815e8761411ea9d5fae645e907d9ea7
LEEJUHUI <83942393+J00HUI@users.noreply.github.com>
2022-06-04 21:25:00 +0900

    Update nqueenback.java

--- nqueenback.java
+++ nqueenback.java
@@ -38,3 +38,52 @@ public class NQueenBackTrackingTest {
 	}
 
 }
+
+
+================================================================================================================================
+public class NQueenBackTrackingTest2 {
+
+	static int N, ans;
+	static int col[];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		ans = 0;
+		col = new int[N+1];
+		
+//		if(isAvailable(0)) {  // 원래 이렇게 해줘야 하나 0 퀸은 무조건 유망하기 때문에 뺴줘도 됨
+//			setQueen(1);
+//		}
+		setQueen(1);
+		System.out.println(ans);
+	}
+	
+	public static void setQueen(int rowNo) { // rowNo : 퀸을 두어야하는 현재 행
+		
+		// 기저조건 : 퀸을 모두 놓았다면
+		if(rowNo > N) {
+			ans++;
+			return;
+		}
+		
+		// 1열부터 - n열까지 퀸을 놓는 시도
+		for (int i = 1; i <= N; i++) {
+			col[rowNo] = i;
+			
+			if(isAvailable(rowNo)) {
+				setQueen(rowNo+1);
+			}
+		}
+	}
+	
+	public static boolean isAvailable(int rowNo) {	// rowNo : 놓아진 마지막 퀸의 행
+		
+		for (int i = 1; i < rowNo; i++) {
+			if(col[rowNo] == col[i] || rowNo-i == Math.abs(col[rowNo] - col[i])) return false;
+		}
+		
+		return true;
+	}
+
+}
