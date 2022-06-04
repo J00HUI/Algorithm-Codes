4601598ac50fe467f8f317fa40ab9d1ac11e45a2
LEEJUHUI <83942393+J00HUI@users.noreply.github.com>
2022-06-04 20:44:31 +0900

    Create subSet_bit.java

---
+++ subSet_bit.java
@@ -0,0 +1,30 @@
+public class SubSetBinaryCountingTest {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] input = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			input[i] = sc.nextInt();
+		}
+		
+		generateSubSet(input);
+}
+
+	private static void generateSubSet(int[] input) {
+		int N = input.length;
+		
+		for(int flag=0, caseCount = 1<<N ; flag < caseCount; flag++) {
+			// flag : 원소들의 선택사태의 비트맵
+			for(int i=0; i<N; i++) {	// 각 비트열의 상태를 확인
+				if((flag & 1<<i) !=0 ) {
+					System.out.print(input[i]+ " ");
+				}
+			}
+			
+			System.out.println();
+		}
+	}
+	
+}
